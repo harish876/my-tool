d57d7865ce9b4180dce636dbcbd9a97266821f20
harish876 <harishgokul01@gmail.com>
2022-05-31 17:41:07 +0530

    version check done along with file output

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import XLSX from "xlsx";
 import fs from "fs";
-//const sampleApp=require("./config/dyte-js-sample-app.json");
-//const reactApp=require("./config/dyte-react-sample-app.json");
-//const sampleAppBackend=require("./config/dyte-js-sample-app-backend.json");
-function cli(filename) {
+import createWriter from 'csv-writer';
+function parse(filename) {
  const excelData=XLSX.readFile(filename);
  return Object.keys(excelData.Sheets).map((name)=>({
   name,
@@ -12,6 +10,36 @@ function cli(filename) {
  }));
 };
 
+function writeIntoCSVFile(finalResult)
+{
+    const createCsvWriter=createWriter.createObjectCsvWriter;
+    const writer=createCsvWriter({
+        path:'./bin/result.csv',
+        header:[
+            {id:'Name',title:'Name'},
+            {id:'repo',title:'Repo'},
+            {id:'version',title:'Version'},
+            {id:'versionSatisfied',title:'Version-Satisfied'}
+        ]
+    });
+    writer.writeRecords(finalResult).then(()=>{console.log("Data Written onto File")});
+}
+async function helper(dependency,version)
+{
+    const jsonFiles=["./config/dyte-react-sample-app.json","./config/dyte-js-sample-app.json","./config/dyte-sample-app-backend.json"]
+    var data = [];
+    for(var i=0;i<jsonFiles.length;i++)
+    {
+        
+        const d=fs.readFileSync(jsonFiles[i],'utf-8');
+        const fileData=JSON.parse(d);
+        var newData={version:"",versionSatisfied:""};
+        newData.version=fileData.dependencies[dependency].substring(1);
+        newData.versionSatisfied=(newData.version>=version)?"true":"false";
+        data=[...data,newData]
+    }
+    return data;
+}
 const argument=process.argv[2];
 if(argument=='-i')
 {
@@ -21,37 +49,32 @@ if(argument=='-i')
     const version=input[1];
     console.log("The dependency to be checked is:"+dependency);
     console.log("The version to be checked is:"+version);
-
-
-
     if(file=="test")
-    {
-        //convert all the json files to js object notation
-        fs.readFile('./config/dyte-react-sample-app.json','utf-8',(err,jsonString)=>{
-            if(err)
-                console.log(err);
-            else
-            {
-                const data=JSON.parse(jsonString);
-                if(dependency=="axios")
+    {   
+            var sheetData=[];
+            parse("./bin/test.csv").forEach((element)=>{                
+                for(var i=0;i<element.data.length;i++)
                 {
-                    const value=data.dependencies.axios; 
-                    const newValue=value.substring(1);
-                    if(version>newValue)
-                        console.log("New Version Available");
-                    else if(version<newValue)
-                        console.log("Update Required");
-                    else
-                        console.log("Up to date");
-                    
-                    console.log(newValue);
-                        
+                    var name=element.data[i].Name;
+                    var repoName=element.data[i].repo;
+                    sheetData=[...sheetData,{name,repoName}];
                 }
+            });
+            var result=await helper(dependency,version);
+            //merge sheetData and result and write into result.csv
+            var finalCSVResult=[];
+            for(var i=0;i<sheetData.length;i++)
+            {
+                finalCSVResult=[...finalCSVResult,{
+                    Name:sheetData[i].name,
+                    repo:sheetData[i].repoName,
+                    version:result[i].version,
+                    versionSatisfied:result[i].versionSatisfied
+                }]
             }
-        });        
-       /*cli("./bin/test.xlsx").forEach((element)=>{
-            console.log(element.data);
-         });*/
+            //console.log(finalCSVResult);
+            writeIntoCSVFile(finalCSVResult);
+
     }
     else
     {
